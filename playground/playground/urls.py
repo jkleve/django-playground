4a40b3947748271f4819af8754df0135166d6622
Jesse Kleve <jesse.kleve@gmail.com>
2018-05-27 19:58:06 -0600

    redirect root to store

--- playground/playground/urls.py
+++ playground/playground/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import redirect_root
+
 urlpatterns = [
+    path('', redirect_root),
     path('store/', include('store.urls')),
     path('admin/', admin.site.urls),
 ]
